a35557b1606ba7a4ed60bd8222ca18f3ea2122d1
YoungHyeong Ryu <wangta69@naver.com>
2019-06-13 11:21:29 +0900

    Summary

--- src/lib/ng-easing.service.ts
+++ src/lib/ng-easing.service.ts
@@ -7,37 +7,39 @@ export class NgEasingService {
 
     EasingFunctions = {
         // no easing, no acceleration
-        linear: function (t) { return t; },
+        linear: function (t: number) { return t; },
         // accelerating from zero velocity
-        easeInQuad: function (t) { return t * t; },
+        easeInQuad: function (t: number) { return t * t; },
         // decelerating to zero velocity
-        easeOutQuad: function (t) { return t * (2 - t); },
+        easeOutQuad: function (t: number) { return t * (2 - t); },
         // acceleration until halfway, then deceleration
-        easeInOutQuad: function (t) { return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t; },
+        easeInOutQuad: function (t: number) { return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t; },
         // accelerating from zero velocity
-        easeInCubic: function (t) { return t * t * t; },
+        easeInCubic: function (t: number) { return t * t * t; },
         // decelerating to zero velocity
-        easeOutCubic: function (t) { return (--t) * t * t + 1; },
+        easeOutCubic: function (t: number) { return (--t) * t * t + 1; },
         // acceleration until halfway, then deceleration
-        easeInOutCubic: function (t) { return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; },
+        easeInOutCubic: function (t: number) { return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; },
         // accelerating from zero velocity
-        easeInQuart: function (t) { return t * t * t * t; },
+        easeInQuart: function (t: number) { return t * t * t * t; },
         // decelerating to zero velocity
-        easeOutQuart: function (t) { return 1 - (--t) * t * t * t; },
+        easeOutQuart: function (t: number) { return 1 - (--t) * t * t * t; },
         // acceleration until halfway, then deceleration
-        easeInOutQuart: function (t) { return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t; },
+        easeInOutQuart: function (t: number) { return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t; },
         // accelerating from zero velocity
-        easeInQuint: function (t) { return t * t * t * t * t; },
+        easeInQuint: function (t: number) { return t * t * t * t * t; },
         // decelerating to zero velocity
-        easeOutQuint: function (t) { return 1 + (--t) * t * t * t * t; },
+        easeOutQuint: function (t: number) { return 1 + (--t) * t * t * t * t; },
         // acceleration until halfway, then deceleration
-        easeInOutQuint: function (t) { return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t; },
+        easeInOutQuint: function (t: number) { return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t; },
         // elastic bounce effect at the beginning
-        easeInElastic: function (t) { return (.04 - .04 / t) * Math.sin(25 * t) + 1; },
+        easeInElastic: function (t: number) { return (.04 - .04 / t) * Math.sin(25 * t) + 1; },
         // elastic bounce effect at the end
-        easeOutElastic: function (t) { return .04 * t / (--t) * Math.sin(25 * t); },
+        easeOutElastic: function (t: number) { return .04 * t / (--t) * Math.sin(25 * t); },
         // elastic bounce effect at the beginning and end
-        easeInOutElastic: function (t) { return (t -= .5) < 0 ? (.01 + .01 / t) * Math.sin(50 * t) : (.02 - .01 / t) * Math.sin(50 * t) + 1; }
+        easeInOutElastic: function (t: number) {
+            return (t -= .5) < 0 ? (.01 + .01 / t) * Math.sin(50 * t) : (.02 - .01 / t) * Math.sin(50 * t) + 1;
+        }
     };
 
     constructor() {}
@@ -52,7 +54,7 @@ export class NgEasingService {
     * @Param CallBackFunciton onUpdate  fired on progressing
     * @Param CallBackFunciton onComplete fired on Finished;
     */
-    startAnimation(params, onUpdate, onComplete) {
+    startAnimation(params: any, onUpdate: (body: any) => void, onComplete: (body: any) => void) {
         // Linear interpolation
         const duration = params.duration;
         const from = params.from;
@@ -60,7 +62,7 @@ export class NgEasingService {
         const ease = params.ease;
         const delay = params.delay;
 
-        const lerp = (source, target, amount) => source + amount * (target - source);
+        const lerp = (source: number, target: number, amount: number) => source + amount * (target - source);
         // Animation start time
         const start = Date.now() - delay;
 
@@ -85,7 +87,7 @@ export class NgEasingService {
     * duration : milisecond
     * delay : milisecond
     */
-    anmaionStart(params, onUpdate, onComplete?) {
+    anmaionStart(params: any, onUpdate: (body: any) => void, onComplete?: (body: any) => void) {
 
         const checkNum = n => typeof n === 'number' ? n : null;
         const checkFunc = f => typeof f === 'function' ? f : _ => _;
